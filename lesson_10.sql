cdd8fcbb56e5a078beedc02794b132088de5bbac
Igor Pochatkin <mail501@mail.ru>
2020-10-23 12:12:49 +0300

    Задания к 10му уроку

---
+++ lesson_10.sql
@@ -0,0 +1,88 @@
+mysql -uroot -p123456 -P3360
+use shop_online;
+
+
+/* 1. Создайте таблицу logs типа Archive. Пусть при каждом создании записи в таблицах users, 
+ catalogs и products в таблицу logs помещается время и дата создания записи, название таблицы,
+ идентификатор первичного ключа и содержимое поля name.*/
+
+
+DROP TABLE IF EXISTS logs;
+CREATE TABLE logs (
+	created_at DATETIME NOT NULL,
+	table_name VARCHAR(250) NOT NULL,
+	str_id BIGINT(30) NOT NULL,
+	name_value VARCHAR(250) NOT NULL
+) ENGINE = ARCHIVE;
+
+
+
+DROP TRIGGER IF EXISTS watchlog_users;
+delimiter //
+CREATE TRIGGER watchlog_users AFTER INSERT ON users
+FOR EACH ROW
+BEGIN
+	INSERT INTO logs (created_at, table_name, str_id, name_value)
+	VALUES (NOW(), 'users', NEW.id, NEW.name);
+END //
+delimiter ;
+
+
+
+DROP TRIGGER IF EXISTS watchlog_catalogs;
+delimiter //
+CREATE TRIGGER watchlog_catalogs AFTER INSERT ON catalogs
+FOR EACH ROW
+BEGIN
+	INSERT INTO logs (created_at, table_name, str_id, name_value)
+	VALUES (NOW(), 'catalogs', NEW.id, NEW.name);
+END //
+delimiter ;
+
+
+
+delimiter //
+CREATE TRIGGER watchlog_products AFTER INSERT ON products
+FOR EACH ROW
+BEGIN
+	INSERT INTO logs (created_at, table_name, str_id, name_value)
+	VALUES (NOW(), 'products', NEW.id, NEW.name);
+END //
+delimiter ;
+
+
+
+-- 2. (по желанию) Создайте SQL-запрос, который помещает в таблицу users миллион записей.
+
+
+DROP TABLE IF EXISTS test_users; 
+CREATE TABLE test_users (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(255),
+	birthday_at DATE,
+	`created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+ 	`updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+
+DROP PROCEDURE IF EXISTS insert_into_users ;
+delimiter //
+CREATE PROCEDURE insert_into_users ()
+BEGIN
+	DECLARE i INT DEFAULT 100;
+	DECLARE j INT DEFAULT 0;
+	WHILE i > 0 DO
+		INSERT INTO test_users(name, birthday_at) VALUES (CONCAT('user_', j), NOW());
+		SET j = j + 1;
+		SET i = i - 1;
+	END WHILE;
+END //
+delimiter ;
+
+
+-- test
+SELECT * FROM test_users;
+
+CALL insert_into_users();
+
+SELECT * FROM test_users LIMIT 3;
